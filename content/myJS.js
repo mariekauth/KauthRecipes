a53d7c9edfc77bc7ce8d3bf99d46b25970ed6d36
Marie Kauth <marie.kauth@gmail.com>
2021-09-19 13:06:36 -0400

    Add original copied content

---
+++ content/myJS.js
@@ -0,0 +1,15 @@
+﻿$(document).ready(function(){
+  $("li").mouseover(function(){
+		$(this).css('background-color','#FFFFCC');
+		$(this).css('color','#8F4E8F');
+		$(this).css( 'cursor', 'pointer' );
+  });
+  $("li").mouseout(function(){
+		$(this).css('background-color','#8F4E8F');
+		$(this).css('color','#FFFFCC');
+  });
+  $("li").click(function(){
+    var mySelection = $(this).text().replace(/ /g,'') + '.htm';
+    $('#content').load('content/' + mySelection);
+  });
+});
